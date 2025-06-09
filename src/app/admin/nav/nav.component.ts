d13c26785480103b846da6d10d083ff2c9da33df
hibaaahhh <hibakalathil02@gmail.com>
2025-06-09 16:58:38 +0530

    mean blog

--- src/app/admin/nav/nav.component.ts
+++ src/app/admin/nav/nav.component.ts
@@ -5,7 +5,6 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrl: './nav.component.css'
 })
 export class NavComponent {
   showMenu = false;
